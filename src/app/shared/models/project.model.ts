06323c7c8a9932fbef5de5e8657513eb3db3e6ec
epdesai <piyushkumar.desai@altran.com>
2019-02-05 16:10:24 +0000

    [Angular-redux][Front-end][Web]  Update project and task models - Development

--- src/app/shared/models/project.model.ts
+++ src/app/shared/models/project.model.ts
@@ -2,13 +2,13 @@ import { Task } from './task.model';
 
 export class Project {
     constructor(
-        public _id: string,
-        public name: string,
-        public _user: string,
-        public __v: number,
-        public updatedAt: Date,
-        public createAt: Date,
-        public tasks: Array<Task>,
-        public slug: string
+        public _id?: string,
+        public name?: string,
+        public _user?: string,
+        public __v?: number,
+        public updatedAt?: Date,
+        public createAt?: Date,
+        public tasks?: Array<Task>,
+        public slug?: string
     ) {}
 }
--- src/app/shared/models/task.model.ts
+++ src/app/shared/models/task.model.ts
@@ -1,11 +1,11 @@
 export class Task {
     constructor(
-        public name: string,
-        public description: string,
-        public _project: string,
+        public name?: string,
+        public description?: string,
+        public _project?: string,
         public _id?: string,
         public updatedAt?: Date,
         public createAt?: Date,
-        public complete?: number
+        public complete?: boolean
     ) {}
 }
