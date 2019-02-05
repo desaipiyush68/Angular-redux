8b7468a3c8740b98941297ddfaad9ec489cc3d83
epdesai <piyushkumar.desai@altran.com>
2019-02-05 16:09:49 +0000

    [Angular-redux][Front-end][Web]  Add tasks actions in to project actions  - Development

--- src/app/actions/project.actions.ts
+++ src/app/actions/project.actions.ts
@@ -1,4 +1,6 @@
 import { Action } from '@ngrx/store';
+import { Project } from '../shared/models/project.model';
+import { Task } from '../shared/models/task.model';
 
 export const GET_PROJECT_LIST = 'Project get list';
 export const GET_PROJECT_LIST_SUCCESS = 'Project get list success';
@@ -8,43 +10,78 @@ export const UPDATE_PROJECT = 'Project update';
 export const UPDATE_PROJECT_SUCCESS = 'Project update success';
 export const DELETE_PROJECT = 'Project delete';
 export const DELETE_PROJECT_SUCCESS = 'Project delete success';
+export const CREAT_TASK = 'Task create';
+export const CREAT_TASK_SUCCESS = 'Task create success';
+export const UPDATE_TASK = 'Task update';
+export const UPDATE_TASK_SUCCESS = 'Task update success';
+export const DELETE_TASK = 'Task delete';
+export const DELETE_TASK_SUCCESS = 'Task delete success';
 
 export class GetProjectList implements Action {
     readonly type = GET_PROJECT_LIST;
-    constructor(public payload?: any) {}
+    constructor(public payload?: Array<Project>) {}
 }
 
 export class GetProjectListSuccess implements Action {
     readonly type = GET_PROJECT_LIST_SUCCESS;
-    constructor(public payload: any) {}
+    constructor(public payload: Array<Project>) {}
 }
 
 export class CreateProject implements Action {
     readonly type = CREAT_PROJECT;
-    constructor(public payload: any) {}
+    constructor(public payload: Project) {}
 }
 
 export class CreateProjectSuccess implements Action {
     readonly type = CREAT_PROJECT_SUCCESS;
-    constructor(public payload?: any) {}
+    constructor(public payload: Project) {}
 }
 
 export class UpdateProject implements Action {
     readonly type = UPDATE_PROJECT;
-    constructor(public payload: any) {}
+    constructor(public payload: Project) {}
 }
 export class UpdateProjectSuccess implements Action {
     readonly type = UPDATE_PROJECT_SUCCESS;
-    constructor(public payload?: any) {}
+    constructor(public payload: Project) {}
 }
 
 export class DeleteProject implements Action {
     readonly type = DELETE_PROJECT;
-    constructor(public payload: any) {}
+    constructor(public payload: Project) {}
 }
 
 export class DeleteProjectSuccess implements Action {
     readonly type = DELETE_PROJECT_SUCCESS;
+    constructor(public payload?: any) {}
+}
+
+export class CreateTask implements Action {
+    readonly type = CREAT_TASK;
+    constructor(public payload: Task) {}
+}
+
+export class CreateTaskSuccess implements Action {
+    readonly type = CREAT_TASK_SUCCESS;
+    constructor(public payload: any) {}
+}
+
+export class UpdateTask implements Action {
+    readonly type = UPDATE_TASK;
+    constructor(public payload: Task) {}
+}
+export class UpdateTaskSuccess implements Action {
+    readonly type = UPDATE_TASK_SUCCESS;
+    constructor(public payload: any) {}
+}
+
+export class DeleteTask implements Action {
+    readonly type = DELETE_TASK;
+    constructor(public payload: Task) {}
+}
+
+export class DeleteTaskSuccess implements Action {
+    readonly type = DELETE_TASK_SUCCESS;
     constructor(public payload?: any) {}
 }
 
@@ -56,4 +93,10 @@ export type All =
     | UpdateProject
     | UpdateProjectSuccess
     | DeleteProject
-    | DeleteProjectSuccess;
+    | DeleteProjectSuccess
+    | CreateTask
+    | CreateTaskSuccess
+    | UpdateTask
+    | UpdateTaskSuccess
+    | DeleteTask
+    | DeleteTaskSuccess;
