87aeb1e2a661f209ac60377769f3c85490467da0
epdesai <piyushkumar.desai@altran.com>
2019-02-05 16:08:27 +0000

    [Angular-redux][Front-end][Web]  Update store - Development

--- src/app/store/store.ts
+++ src/app/store/store.ts
@@ -1,10 +1,8 @@
-
 import { Task } from './../shared/models/task.model';
 import { User } from './../shared/models/user.model';
 import { Project } from './../shared/models/project.model';
 
 export interface AppState {
-  user: User;
-  project: Project;
-  task: Task;
+    user: User;
+    project: Project;
 }
